861c8f9c0687133729b762405db9a55314ca21a3
princeblack <mahamadicongo@hotmail.com>
2020-12-07 10:42:03 +0100

    update cors

--- app.js
+++ app.js
@@ -8,7 +8,17 @@ const cors = require("cors");
 const app = express();
 
 // allow cross-origine requests
-app.use(cors())
+app.use(cors(
+  {
+    origin: ["*","http://localhost:3000"],
+    allowedHeaders:'Origin, X-Requested-With, Content, Accept,Content-Type,Authorization',
+    credentials: true,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false,
+    optionsSuccessStatus: 204 // some legacy browsers (IE11, various SmartTVs) choke on 204
+
+  }
+))
 // connect to mongoDb database
 mongoose.connect(
   "mongodb+srv://princeblack:Africadmc01@cluster0.cdxps.mongodb.net/<dbname>?retryWrites=true&w=majority",
